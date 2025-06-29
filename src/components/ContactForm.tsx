8c9091209fc1fc891c1817b4be6be5fbc5176a3f
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2025-06-29 15:00:22 +0530

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -88,7 +88,7 @@ export default function QuoteRequestForm() {
     return newErrors;
   };
 
-  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>)) => {
+  const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const newErrors = validateForm();
