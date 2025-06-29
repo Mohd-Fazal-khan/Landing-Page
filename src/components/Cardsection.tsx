0b28ae6c2332b9ec42fae4ca552bb397babe9547
Fazal Khan <124332727+Mohd-Fazal-khan@users.noreply.github.com>
2025-06-29 14:41:37 +0530

    Update Cardsection.tsx

--- src/components/Cardsection.tsx
+++ src/components/Cardsection.tsx
@@ -19,7 +19,7 @@ const Cardsection = () => {
     visible: {
       opacity: 1,
       y: 0,
-      transition: { duration: 0.6, ease: 'easeOut' },
+      transition: { duration: 0.6, ease: [0.42, 0, 0.58, 1] },
     },
   };
 
